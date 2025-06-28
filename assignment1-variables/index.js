df9087d6365b4d6b1b4ebceb3f26c04f1503632c
Tejas Soni <tejas.tejas.soni3@gmail.com>
2025-06-28 14:33:09 +0530

    [ASGN-1] added different types of variables

---
+++ assignment1-variables/index.js
@@ -0,0 +1,9 @@
+var userName = 'John Doe';
+var userAge30 = 30;
+var $specialChar = '!';
+var _underscoreVar = 'This is a valid variable name';
+
+console.log(userName); // Output: John Doe
+console.log(userAge30); // Output: 30
+console.log($specialChar); // Output: !
+console.log(_underscoreVar); // Output: This is a valid variable name       
